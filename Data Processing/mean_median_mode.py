43636b548472cce387b490795b7df69b6c09a080
LifeIs-Unfair <forfaltukaam69@gmail.com>
2025-05-21 12:05:27 +0530

    Mean, median,mode prog

---
+++ Data Processing/mean_median_mode.py
@@ -0,0 +1,33 @@
+#Mean, Median, Mode Calculation
+#Mean
+def calculate_mean(data):
+    """Calculate the mean of a list of numbers."""
+    if not data:
+        return 0
+    return sum(data) / len(data)
+data_set = [1,2,3,4,5]
+mean_value = calculate_mean(data_set)
+print(f"Mean: {mean_value}")
+
+#Median
+def calculate_median(data):
+    """Calculate the median of a list of numbers."""
+    data.sort()
+    n = len(data)
+    mid = n//2
+    if n % 2 == 0:
+        median = (data[mid - 1] + data[mid]) / 2  
+    else:
+        median = data[mid]  
+    return median
+data_set = [1,2,3,4,5]
+median_value = calculate_mean(data_set)
+print(f"Median: {median_value}")
+
+#Mode
+from statistics import mode
+def calculate_mode(data):
+    """Calculate the mode of a list of numbers."""
+data_set = [1,2,3,4,4,4,5,5,5,5,5,5]
+mode_value = mode(data_set)
+print(f"Mode: {mode_value}")
